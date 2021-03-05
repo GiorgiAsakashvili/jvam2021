1dea4fd024312b75a6100c20fedb72c961740b74
Giorgi Asakashvili <55878120+GiorgiAsakashvili@users.noreply.github.com>
2021-03-05 19:36:34 +0400

    W1 - Ex3

---
+++ Practice1/src/gau/Main.java
@@ -0,0 +1,14 @@
+package gau;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //    Exercise3 exercise3 = new Exercise3();
+        //    Exercise6 exercise6 = new Exercise6();
+        //    Exercise9 exercise9 = new Exercise9();
+        Exercise12 exercise12 = new Exercise12();
+    }
+}
